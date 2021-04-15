69cd880e31d2a34d41fe3b1bf715be3859bebb8c
Clint <55072157+clintonbf@users.noreply.github.com>
2021-04-15 13:54:10 -0700

    minor: formatting and usage of costants

--- app/src/main/java/ca/bcit/udpchat/Environment.java
+++ app/src/main/java/ca/bcit/udpchat/Environment.java
@@ -3,28 +3,16 @@ package ca.bcit.udpchat;
 import android.media.AudioFormat;
 
 public class Environment {
-    public static final String SERVER  = "192.168.1.76";
-    public static final int PORT       = 65432;
-    public static final int       SAMPLE_RATE     = 10000;                                //samples/second
-    public static final int       CHANNELS        = 1;                                    //mono
-    public static final int       BIT_DEPTH       = AudioFormat.ENCODING_PCM_16BIT;       //bits, I think
-    public static final double    TIME_FRAME      = 0.5;                                  //seconds
-    public static final int       BUFFER_FACTOR   = 10;
-    public static final int       SAMPLE_SIZE     = 2;
-    public static final int       SAMPLE_INTERVAL = 20;                                   //units??
-    public static final int       BUF_SIZE        = SAMPLE_INTERVAL * SAMPLE_INTERVAL * SAMPLE_SIZE * 2;
+    public static final String  SERVER          = "192.168.1.76";
+    public static final int     PORT            = 42069;
+    public static final int     SAMPLE_RATE     = 10000;                         //samples/second
+    public static final int     CHANNELS        = 1;                             //mono
+    public static final int     BIT_DEPTH       = AudioFormat.ENCODING_PCM_16BIT;//bits, I think
+    public static final double  TIME_FRAME      = 0.5;                           //seconds
+    public static final int     BUFFER_FACTOR   = 10;
+    public static final int     SAMPLE_SIZE     = 2;
+    public static final int     SAMPLE_INTERVAL = 20;                            //units??
+    public static final int     BUF_SIZE        = SAMPLE_INTERVAL * SAMPLE_INTERVAL * SAMPLE_SIZE * 2;
 
-    public Environment() {
-
-    }
-
-    public static String getServer() {
-        return SERVER;
-    }
-
-    public static int getPort() {
-        return PORT;
-    }
-
-
+    public Environment() { }
 }
